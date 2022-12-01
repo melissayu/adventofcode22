edbbe981125ff957335cc527c6c56222b1388288
Melissa Dirdo <melissa.y00@gmail.com>
2022-12-01 14:42:50 -0800

    advent of code - day 1

---
+++ day1/dec_1.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const readline = require('readline');
+
+void (async () => {
+  const rl = readline.createInterface({
+    input: fs.createReadStream('dec_1_input.txt'),
+    crlfDelay: Infinity,
+  });
+
+  let elfTotals = [];
+  let currentElfTotal = 0;
+  
+  //for each line
+  rl.on('line', (line) => {
+
+    //if blank => end of elf total
+    if(line == ''){
+        //add to elfTotals
+        elfTotals.push(parseInt(currentElfTotal));
+        //reset currentElfTotal to 0
+        currentElfTotal = 0;
+    } 
+    //else (not blank)
+    else {
+        //add value to currentElfTotal
+        currentElfTotal+= parseInt(line);
+    }
+});
+
+  await new Promise((res) => {
+    rl.once('close', res);
+    });
+    //part 1: max elf total
+    console.log('topMax: ', Math.max(...elfTotals));
+    //part 2: sum of top 3 elf totals
+    const topThree = elfTotals.sort(function(a, b){return b-a}).slice(0,3);
+    const topThreeSum = topThree.reduce((a, b) => a+b);
+    console.log('topThreeSum: ', topThreeSum);
+
+
+})();
